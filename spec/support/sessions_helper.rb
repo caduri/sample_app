6c9afb0a69748d23bfa8e8036d79c7d33699e65b
Caduri Katzav <cadurik@gmail.com>
2013-04-16 22:42:46 +0300

    Finish user edit, update, index, and destroy actions

--- spec/support/sessions_helper.rb
+++ spec/support/sessions_helper.rb
@@ -6,8 +6,17 @@ module SessionsHelper
 	  fill_in "Confirmation", with: "foobar"
 	end
 
-	def valid_signin(user)
-		fill_in "Email",    with: user.email.upcase
+	def valid_signin(user, options = {})
+		if options[:visit]
+			visit options[:visit]
+		else
+			visit signin_path
+		end
+		
+	  fill_in "Email",    with: user.email
 	  fill_in "Password", with: user.password
+	  click_button "Sign in"
+	  # Sign in when not using Capybara as well.
+	  cookies[:remember_token] = user.remember_token
 	end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,16 +1,5 @@
 include ApplicationHelper
-
-def valid_signup()
-	fill_in "Name",         with: "Example User"
-  fill_in "Email",        with: "user@example.com"
-  fill_in "Password",     with: "foobar"
-  fill_in "Confirmation", with: "foobar"
-end
-
-def valid_signin(user)
-	fill_in "Email",    with: user.email.upcase
-  fill_in "Password", with: user.password
-end
+include SessionsHelper
 
 #def full_title(page_title)
 #  base_title = "Ruby on Rails Tutorial Sample App"
